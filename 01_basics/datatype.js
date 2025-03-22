4f63ca547a80e54772e9530d683632dd43e78cc9
Suyog Nagbhide <146908323+suyog358@users.noreply.github.com>
2025-03-22 04:23:51 +0000

    datatype

---
+++ 01_basics/datatype.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+// alert( 2 + 2) for the pop in browser console
+
+console.log(2+3)
+
+let name = "suyog"
+let age = 22
+let isloggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolen => true/false
+// null => standlone value
+// undifined => variable assing but not a put value
+// symbol => when intro uniques
+
+// objects
+
+console.log(typeof"sharu")
+console.log(typeof age)
+console.log(typeof null) // this is object
+console.log(typeof undefined) // this is type 
---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+const accountID = 143
+let accountEmail = "suyog@gmail.com"
+var accountPassword = "12233"
+accountCity = "pune"
+
+// accountID = 2 
+
+accountEmail = "sharu@gmail.com"
+accountPassword = "143"
+accountCity = "mumbai"
+
+console.table([accountID, accountEmail, accountPassword, accountCity])
+
+console.log(accountID);
