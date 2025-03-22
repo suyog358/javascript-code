4edace46833e898643f88f9ae423ca6b7adf8197
Suyog Nagbhide <146908323+suyog358@users.noreply.github.com>
2025-03-22 14:20:05 +0000

    conversion of data

---
+++ 01_basics/conversion.js
@@ -0,0 +1,20 @@
+let score = "33love"
+console.log(typeof score)
+
+let valueInNumber = Number(score) // convert string into number
+console.log(valueInNumber)
+console.log(score)
+console.log(typeof valueInNumber)
+
+
+let someNumber = 45
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber); // covert number into string 
+
+
+let whatNumber = "" // convert false and "suyog" => convert into True
+let converInboolen = Boolean(whatNumber) // convert 1 into True
+console.log(converInboolen)
+console.log(whatNumber)
+
