0d2c04513212c8e419f0f5e9ba5e2a3b0f603e53
Suyog Nagbhide <146908323+suyog358@users.noreply.github.com>
2025-03-22 15:01:38 +0000

    opreation

---
+++ 01_basics/opreations.js
@@ -0,0 +1,25 @@
+let value = 35
+let negValue = -value
+console.log(negValue);
+console.log(typeof negValue);
+
+/*
+console.log(2+2)
+console.log(3*4)
+console.log(4**5)
+*/
+
+let str1 = "suyog"
+let str2 = " sharu"
+
+let str3 = str1 + str2
+console.log(str3)
+
+
+console.log(true);
+console.log(+true);
+console.log(+"")
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter)
